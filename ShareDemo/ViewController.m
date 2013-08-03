0cc0843c08e2a11265fb8f4368ad31e1927e4cb5
Jikui Peng <pjk1129@163.com>
2013-08-03 14:47:27 +0800

    客户端未安装时，提示用户

--- ShareDemo/ViewController.m
+++ ShareDemo/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 #import "ShareManager.h"
+#import "WXApi.h"
+#import "WXApiObject.h"
 
 @interface ViewController ()
 
@@ -27,56 +29,78 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)shareToWX:(id)sender {    
-    [[ShareManager sharedManager] sendTextContentToWX:@"即使缤纷落尽，繁华消亡，也不要被生活磨平了棱角"
-                                                scene:WXSceneTypeSession
-                                      completionBlock:^(ShareManager *manager) {
-                                          NSLog(@"======成功：分享文本到朋友=======");
-   
-                                      } failedBlock:^(ShareManager *manager) {
-                                          NSLog(@"======失败：分享图片到朋友=======");
+- (BOOL)isWXAppInstalled{
+    if (![WXApi isWXAppInstalled]) {
+        UIAlertView *msgbox = [[UIAlertView alloc] initWithTitle:@"Error" message:@"未安装微信客户端" delegate:nil cancelButtonTitle:@"取消" otherButtonTitles:nil];
+        [msgbox show];
+        [msgbox release];
+        return NO;
+    }
+    return YES;
+}
 
-                                      }];
+- (IBAction)shareToWX:(id)sender {
+    if ([self isWXAppInstalled]) {
+        [[ShareManager sharedManager] sendTextContentToWX:@"即使缤纷落尽，繁华消亡，也不要被生活磨平了棱角"
+                                                    scene:WXSceneTypeSession
+                                          completionBlock:^(ShareManager *manager) {
+                                              NSLog(@"======成功：分享文本到朋友=======");
+                                              
+                                          } failedBlock:^(ShareManager *manager) {
+                                              NSLog(@"======失败：分享图片到朋友=======");
+                                              
+                                          }];
+    }
+    
+
 }
 
 - (IBAction)shareToWX1:(id)sender {
-    [[ShareManager sharedManager] sendTextContentToWX:@"即使缤纷落尽，繁华消亡，也不要被生活磨平了棱角"
-                                                scene:WXSceneTypeTimeline
-                                      completionBlock:^(ShareManager *manager) {
-                                          NSLog(@"======成功：分享文本到朋友圈=======");
+    
+    if ([self isWXAppInstalled]) {
+        [[ShareManager sharedManager] sendTextContentToWX:@"即使缤纷落尽，繁华消亡，也不要被生活磨平了棱角"
+                                                    scene:WXSceneTypeTimeline
+                                          completionBlock:^(ShareManager *manager) {
+                                              NSLog(@"======成功：分享文本到朋友圈=======");
+                                              
+                                              
+                                          } failedBlock:^(ShareManager *manager) {
+                                              NSLog(@"======失败：分享文本到朋友圈=======");
+                                              
+                                          }];
+    }
 
-                                          
-                                      } failedBlock:^(ShareManager *manager) {
-                                          NSLog(@"======失败：分享文本到朋友圈=======");
-
-                                      }];
     
 }
 
-- (IBAction)shareToX2:(id)sender {    
-    [[ShareManager sharedManager] sendImageContentToWX:[UIImage imageNamed:@"1.jpg"]
-                                                scene:WXSceneTypeSession
-                                      completionBlock:^(ShareManager *manager) {
-                                          NSLog(@"======成功：分享图片到朋友=======");
-
-                                          
-                                      } failedBlock:^(ShareManager *manager) {
-                                          NSLog(@"======失败：分享图片到朋友=======");
-
-                                      }];
+- (IBAction)shareToX2:(id)sender {
+    if ([self isWXAppInstalled]) {
+        [[ShareManager sharedManager] sendImageContentToWX:[UIImage imageNamed:@"1.jpg"]
+                                                     scene:WXSceneTypeSession
+                                           completionBlock:^(ShareManager *manager) {
+                                               NSLog(@"======成功：分享图片到朋友=======");
+                                               
+                                               
+                                           } failedBlock:^(ShareManager *manager) {
+                                               NSLog(@"======失败：分享图片到朋友=======");
+                                               
+                                           }];
+    }
 }
 
 - (IBAction)shareToWX3:(id)sender {
-    [[ShareManager sharedManager] sendImageContentToWX:[UIImage imageNamed:@"1.jpg"]
-                                                 scene:WXSceneTypeTimeline
-                                       completionBlock:^(ShareManager *manager) {
-                                           NSLog(@"======成功：分享图片到朋友圈=======");
-
-                                           
-                                       } failedBlock:^(ShareManager *manager) {
-                                           NSLog(@"======失败：分享图片到朋友圈=======");
-
-                                       }];
+    if ([self isWXAppInstalled]) {
+        [[ShareManager sharedManager] sendImageContentToWX:[UIImage imageNamed:@"1.jpg"]
+                                                     scene:WXSceneTypeTimeline
+                                           completionBlock:^(ShareManager *manager) {
+                                               NSLog(@"======成功：分享图片到朋友圈=======");
+                                               
+                                               
+                                           } failedBlock:^(ShareManager *manager) {
+                                               NSLog(@"======失败：分享图片到朋友圈=======");
+                                               
+                                           }];
+    }
 
 }
 
